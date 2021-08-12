6dd0fdfe5d8237724b4a2fa16c62bfe854586453
bricefouepe <bricefouepe@hotmail.com>
2021-08-12 21:27:55 +0200

    first commit

---
+++ src/pages/HomePage/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
+import { InfoSection, Pricing } from '../../components';
+import {Paper} from "@material-ui/core";
+import './home.css';
+
+function Home() {
+  return (
+    <>
+      {/*<InfoSection {...homeObjOne} />*/}
+        <Paper>
+            <img src={require('../../images/banniere.jpg')} alt="banner" />
+        </Paper>
+      <InfoSection {...homeObjThree} />
+      <InfoSection {...homeObjTwo} />
+      <Pricing />
+      {/*<InfoSection {...homeObjFour} />*/}
+    </>
+  );
+}
+
+export default Home;
