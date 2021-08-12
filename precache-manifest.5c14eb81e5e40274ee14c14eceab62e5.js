77bbf38b39082006007928e9bbdd23f4f7ec22c3
bricefouepe <bricefouepe@hotmail.com>
2021-08-12 21:15:27 +0200

    Updates

---
+++ precache-manifest.5c14eb81e5e40274ee14c14eceab62e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68338533c47a4a99c872c3e30d1d3b95",
+    "url": "/asdam/index.html"
+  },
+  {
+    "revision": "41eff2a88e6510884b68",
+    "url": "/asdam/static/css/main.97bedc8c.chunk.css"
+  },
+  {
+    "revision": "50018faa83553b1c761e",
+    "url": "/asdam/static/js/2.6d2ac39c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/asdam/static/js/2.6d2ac39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41eff2a88e6510884b68",
+    "url": "/asdam/static/js/main.4b3fb083.chunk.js"
+  },
+  {
+    "revision": "a80458810060f1cc5c4d",
+    "url": "/asdam/static/js/runtime-main.8938a3da.js"
+  },
+  {
+    "revision": "7d218cc654b6469370493a54f680dbeb",
+    "url": "/asdam/static/media/banniere.7d218cc6.jpg"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/asdam/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/asdam/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/asdam/static/media/svg-2.d4846e11.svg"
+  }
+]);
